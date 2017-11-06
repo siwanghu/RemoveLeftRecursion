1f451b3417561ab114c9beab49a817dbcdd89948
‘siwanghu <siwanghu@hotmail.com>
2017-11-06 12:38:46 +0800

    siwanghu

---
+++ src/com/siwanghu/syntaxanalyzer/bean/Grammar.java
@@ -0,0 +1,48 @@
+package com.siwanghu.syntaxanalyzer.bean;
+
+public class Grammar {
+    private String left;
+    private String right;
+    private int id;
+    private static int ID = 0;
+ 
+    public Grammar() {
+        super();
+        id = ID++;
+    }
+ 
+    public Grammar(String left, String right) {
+        super();
+        this.left = left;
+        this.right = right;
+        id = ID++;
+    }
+ 
+    public String getLeft() {
+        return left;
+    }
+ 
+    public void setLeft(String left) {
+        this.left = left.replace(" ", "");
+    }
+ 
+    public String getRight() {
+        return right;
+    }
+ 
+    public void setRight(String right) {
+        this.right = right.replace(" ", "");
+    }
+ 
+    public int getId() {
+        return id;
+    }
+     
+     
+ 
+    @Override
+    public String toString() {
+        return "Grammar [left=" + left + ", right=" + right + "]";
+    }
+ 
+}
